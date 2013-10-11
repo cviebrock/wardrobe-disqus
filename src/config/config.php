eb6214e4a5119fcc08b6527290dd47e7a1488c0c
Colin Viebrock <colin@viebrock.ca>
2013-10-11 13:42:10 -0500

    remove identifier from config (it is passed to methods instead)

--- src/config/config.php
+++ src/config/config.php
@@ -9,12 +9,4 @@ return array(
 	 */
 	'disqus_shortname' => '',
 
-	/**
-	 * Disqus_use_identifier: If you want to use the post's primary key as
-	 * an identifier for Disqus threads, then set this to true.  Setting it to false
-	 * will cause Disqus to use the URL of the post, which may lead to lost comments
-	 * if you edit the post's URL down the road.
-	 */
-	'disqus_use_identifier' => true,
-
 );
